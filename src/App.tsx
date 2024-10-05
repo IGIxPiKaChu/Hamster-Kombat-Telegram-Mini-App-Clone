7d03f3376d5c4e0ca8477be726f0cd0839ad6767
IGIxPiKaChu <156430877+IGIxPiKaChu@users.noreply.github.com>
2024-10-05 20:10:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,10 +35,6 @@ const App: React.FC = () => {
     1000000000
   ];
 
-  const levelImages = [
-    dailyCombo, 
-  ];
-
   const [levelIndex, setLevelIndex] = useState(6);
   const [points, setPoints] = useState(() => {
     const savedPoints = localStorage.getItem('points');
@@ -183,10 +179,11 @@ const App: React.FC = () => {
             </div>
           </div>
         </div>
+
         <div className="flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0">
           <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]">
             <div className="px-4 mt-6 flex justify-between gap-2">
-              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-flative">
                 <div className="dot"></div>
                 <img src={dailyReward} alt="Daily Reward" className="mx-auto w-12 h-12" />
                 <p className="text-[10px] text-center text-white mt-1">Daily reward</p>
@@ -219,7 +216,7 @@ const App: React.FC = () => {
                 onClick={handleCardClick}
               >
                 <div className="w-full h-full rounded-full circle-inner">
-                  <img src={levelImages[levelIndex]} alt="Character" className="w-full h-full" />
+                  <img src={mainCharacter} alt="Main Character" className="w-full h-full" />
                 </div>
               </div>
             </div>
@@ -270,5 +267,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-          
